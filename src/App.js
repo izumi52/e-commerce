2c48162a12d36a9a458531d422f5a872d19f07e3
izumi-takeshima <o.inzauhmci@gmail.com>
2021-05-01 23:29:27 +1000

    Updating shop page, collection preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -3,15 +3,14 @@ import { Route, Switch } from 'react-router-dom';
 import './App.css';
 
 import Homepage from './pages/homepage/homepage.component';
-const Details = () => (
-  <div>Details</div>
-)
+import ShopPage from './pages/shop/shop.component';
+
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={Homepage} />
-        <Route path='/shop/' component={Details} />
+        <Route path='/shop/' component={ShopPage} />
       </Switch>
     </div>
   );
